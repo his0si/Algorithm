0ed65c2ef1c61e167679a63908de87f0eea09b99
Hee Seo Kim <hisounni@ewhain.net>
2023-05-25 10:17:46 +0900

    Update 숫자 카드 2.py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,4 +1,7 @@
 from collections import Counter
+# Counter 클래스는 collections 모듈에서 제공
+# 입력된 순서대로 요소를 카운트하고 딕셔너리 형태로 반환한다
+# 이를 활용하여 각 숫자 카드의 개수를 세는데 활용 가능
 
 N = int(input())
 card = list(map(int, input().split()))
@@ -11,4 +14,4 @@ ans = []
 for num in Mcard:
     ans.append(card_counter[num])
 
-print(*ans)
+print(*ans) # ans 리스트의 요소들을 공백으로 구분하여 출력
