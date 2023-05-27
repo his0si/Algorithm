25e23e6b07fbb6417eef8d7438eee9fb5437f7e0
Hee Seo Kim <hisounni@ewhain.net>
2023-05-27 15:10:42 +0900

    [Silver II] Title: 과자 나눠주기, Time: 4468 ms, Memory: 148288 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,28 @@
+M, N = map(int, input().split())
+cookies = list(map(int, input().split()))
+
+
+def binary_search(cookies, length):
+    count = 0
+    for cookie in cookies:
+        count = count + cookie // length
+    return count
+    
+cookies.sort()
+
+start = 1
+end = cookies[-1]
+
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = binary_search(cookies, mid)
+    
+    if count >= M:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(result)
