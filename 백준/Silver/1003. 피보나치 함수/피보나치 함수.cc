a92c5467ea0f141797743ff7e7b53429c0ec35bc
Hee Seo Kim <hisounni@ewhain.net>
2023-10-26 10:22:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,31 @@
+//그냥 재귀로 하면 fibonacci 함수의 중복 호출이 많아져 시간 초과
+#include <iostream>
+
+using namespace std;
+
+pair<int, int> dp[41]; 
+
+void countFibo(int n)
+{
+	dp[0] = make_pair(1, 0);
+	dp[1] = make_pair(0, 1); 
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = make_pair((dp[i - 1].first + dp[i - 2].first),
+			(dp[i - 1].second + dp[i - 2].second));
+	}
+}
+
+int main()
+{
+	int T, n;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> n; 
+		countFibo(n); 
+		cout << dp[n].first << ' ' << dp[n].second << '\n'; 
+	}
+
+	return 0;
+}
